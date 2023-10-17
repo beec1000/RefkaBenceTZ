44820c1cfbfa52af0f08403acbb6a8fa5badae0b
beec <beec1000.minecraft@gmail.com>
2023-10-17 13:17:15 +0200

    Add project files.

---
+++ RefkaBenceTZ/Kamion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RefkaBenceTZ
+{
+    class Kamion
+    {
+        public string Marka { get; set; }
+        public string Tipus { get; set; }
+        public int Evjarat { get; set; }
+        public int Loero { get; set; }
+        public double Fogyasztas { get; set; }
+        public int KmperOraAllas { get; set; }
+        //14.feladat
+        public double KW {
+            get
+            {
+                return Loero * 0.7355;
+            }
+        }
+
+        public Kamion(string s)
+        {
+            var v = s.Split(';');
+            this.Marka = v[0];
+            this.Tipus = v[1];
+            this.Evjarat = int.Parse(v[2]);
+            this.Loero = int.Parse(v[3]);
+            this.Fogyasztas = double.Parse(v[4]);
+            this.KmperOraAllas = int.Parse(v[5]);
+        }
+
+        public override string ToString()
+        {
+            return $"{Marka}, {Tipus}, {Evjarat}, {Loero}, {Fogyasztas}, {KmperOraAllas}";
+        }
+
+
+    }
+}
---
+++ RefkaBenceTZ/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace RefkaBenceTZ
+{
+    class Program
+    {
+        static double EvjaratAtlag(List<Kamion> k)
+        {
+            var atlag = k.Average(ave => ave.Evjarat);
+            return atlag;
+        }
+        static int F10(List<Kamion> k)
+        {
+            var MANkaminonok = new List<Kamion>();
+            var f10 = k.Where(d => d.Marka == "MAN" && d.Evjarat >= 2018);
+            foreach (var i in f10)
+            {
+                MANkaminonok.Add(i);
+            }
+            return MANkaminonok.Count();
+        }
+        static Kamion LegtobbetFutott(List<Kamion> k)
+        {
+            var f11 = k.OrderByDescending(m => m.KmperOraAllas).First();
+            return f11;
+        }
+        static Kamion F12(List<Kamion> k)
+        {
+            var mercedes = k.OrderByDescending(d => d.Marka == "Mercedes").First();
+            var keresett = k.Where(d => d.Marka == "Iveco" && d.Fogyasztas < mercedes.Loero).First();
+            return keresett;
+        }
+        static IEnumerable<Kamion> Fogyasztas30lAlatt(List<Kamion> k)
+        {
+            var f13Lista = new List<Kamion>();
+            var fogyasztas = k.Where(d => d.Fogyasztas < 30);
+            foreach (var i in fogyasztas)
+            {
+                f13Lista.Add(i);
+            }
+            return f13Lista;
+        }
+        static void Main(string[] args)
+        {
+            var kamionok = new List<Kamion>();
+
+            using var sr = new StreamReader(@"..\..\..\src\kamionok.txt");
+            _ = sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                kamionok.Add(new Kamion(sr.ReadLine()));
+            }
+
+            Console.WriteLine("7. feladat");
+            foreach (var i in kamionok)
+            {
+                Console.WriteLine(i.ToString());
+            }
+
+            Console.WriteLine("9. feladat");
+            Console.WriteLine($"A kamionok átlagos évjárata: {Math.Round(EvjaratAtlag(kamionok))}");
+
+            Console.WriteLine("10. feladat");
+            Console.WriteLine($"Összesen {F10(kamionok)} db ilyen kamion van.");
+
+            Console.WriteLine("11. feladat");
+            Console.WriteLine($"A legtöbbet futott kamion adatai: {LegtobbetFutott(kamionok)}");
+
+            Console.WriteLine("12. feladat");
+            Console.WriteLine("Az ilyen kamionok adatai: ");
+            if (F12(kamionok) != null)
+            {
+                Console.WriteLine(F12(kamionok).ToString());
+            }
+            else
+            {
+                Console.WriteLine("Nincs ilyen");
+            }
+
+            Console.WriteLine("13. feladat");
+            Console.WriteLine("A 30 liter alatti fogyasztású kamionok adatai: ");
+            foreach (var i in Fogyasztas30lAlatt(kamionok))
+            {
+                Console.WriteLine($"\t{i.ToString()}");
+            }
+
+            Console.WriteLine("15. feladat");
+
+            var Ujkamionok = new List<Kamion>();
+            foreach (var i in kamionok)
+            {
+                Ujkamionok.Add(i);
+            }
+
+            using var sw = new StreamWriter(@"..\..\..\src\Ujkamionok.txt");
+            sw.WriteLine("Márka, Típus, Évjárat, Lóerő, Fogyasztás, Km/Óra állás");
+            foreach (var i in Ujkamionok)
+            {   
+                sw.WriteLine(i.ToString());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
